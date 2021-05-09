f5f5972aea6153e979174b1bdc2f55bf74298d1f
Daniel Angulo <dangulo4@gmail.com>
2021-05-08 18:08:42 -0700

    adding mobile responsive

--- server.js
+++ server.js
@@ -9,12 +9,13 @@ connectDB();
 app.use(express.json({ extended: false }));
 
 // initialize fileUpload
-app.use(fileUpload(), require('./routes/api/images'));
+app.use(fileUpload('uploads'), require('./routes/api/images'));
 
 // test request end point
 app.get('/', (req, res) => res.send('API is running'));
 
 app.use('/api/images', require('./routes/api/images'));
+// app.use(express.static('uploads'), require('./routes/api/images'));
 
 // variable to hold environment
 const PORT = process.env.PORT || 5000;
